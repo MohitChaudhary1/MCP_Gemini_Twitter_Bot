42d99ad1f90969df6f3ba03603f71d14c0c5ee66
Mohit Chaudhary <mohitchaudhary@Mohits-MacBook-Air.local>
2025-04-27 12:57:49 +0530

    First commit - safe code without secrets

---
+++ server.js
@@ -0,0 +1,73 @@
+// server.js
+
+import express from "express";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+import { createPost } from "./mcp.tool.js";
+import { z } from "zod";
+
+const server = new McpServer({
+    name: "example-server",
+    version: "1.0.0"
+});
+
+const app = express();
+
+// Define the tools
+server.tool(
+    "addTwoNumbers",
+    "Add two numbers",
+    {
+        a: z.number(),
+        b: z.number()
+    },
+    async (arg) => {
+        const { a, b } = arg;
+        return {
+            content: [
+                {
+                    type: "text",
+                    text: `The sum of ${a} and ${b} is ${a + b}`
+                }
+            ]
+        }
+    }
+);
+
+server.tool(
+    "createPost",
+    "Create a post on X (Twitter)",
+    {
+        status: z.string()
+    },
+    async (arg) => {
+        const { status } = arg;
+        return createPost(status);
+    }
+);
+
+// Support for multiple clients
+const transports = {};
+
+app.get("/sse", async (req, res) => {
+    const transport = new SSEServerTransport('/messages', res);
+    transports[transport.sessionId] = transport;
+    res.on("close", () => {
+        delete transports[transport.sessionId];
+    });
+    await server.connect(transport);
+});
+
+app.post("/messages", async (req, res) => {
+    const sessionId = req.query.sessionId;
+    const transport = transports[sessionId];
+    if (transport) {
+        await transport.handlePostMessage(req, res);
+    } else {
+        res.status(400).send('No transport found for sessionId');
+    }
+});
+
+app.listen(3001, () => {
+    console.log("Server is running at http://localhost:3001");
+});
